abdc51370328d28b3f9f1c6cb47802e2ebb76e88
Sherlin Tessy P <>
2021-03-30 17:58:48 +0530

    android splash screen not working

--- AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -2,7 +2,7 @@ package com.awesomeproject;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
-import com.cboy.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this);  // here
+      SplashScreen.show(this, R.style.SplashStatusBarTheme);// here
       super.onCreate(savedInstanceState);
   }
   @Override
